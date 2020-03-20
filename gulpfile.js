573fa78714d61c7132e1fd8caeba010f820f31c1
Алексей <agragregra@ya.ru>
2020-03-20 15:46:47 +0700

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 // VARIABLES & PATHS
 
 let preprocessor = 'sass', // Preprocessor (sass, scss, less, styl)
-		fileswatch   = 'html,htm,txt,json,md,woff2', // List of files extensions for watching & hard reload (comma separated)
-		imageswatch  = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
-		baseDir      = 'app', // Base dir path without «/» at the end
-		online       = true; // If «false» - Browsersync will work offline without internet connection
+    fileswatch   = 'html,htm,txt,json,md,woff2', // List of files extensions for watching & hard reload (comma separated)
+    imageswatch  = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
+    baseDir      = 'app', // Base dir path without «/» at the end
+    online       = true; // If «false» - Browsersync will work offline without internet connection
 
 let path = {
 
