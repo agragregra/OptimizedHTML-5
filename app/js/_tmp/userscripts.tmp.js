82a0cff3cc79a67cc29bfc683684524cc9631539
Алексей <agragregra@ya.ru>
2020-11-05 20:43:27 +0300

    Update

--- app/js/_tmp/userscripts.tmp.js
+++ app/js/_tmp/userscripts.tmp.js
@@ -1,4 +1,4 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function () {// Custom JS
+document.addEventListener('DOMContentLoaded', function () {// Custom JS
 });
--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
 
