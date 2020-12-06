86c62da66c57b07ff62f130a1885750ffb07eb8e
Алексей <agragregra@ya.ru>
2020-12-06 08:37:31 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ let fileswatch = 'html,htm,txt,json,md,woff2' // List of files extensions for wa
 
 const { src, dest, parallel, series, watch } = require('gulp')
 const browserSync  = require('browser-sync').create()
+const ssi          = require('browsersync-ssi')
 const webpack      = require('webpack-stream')
 const sass         = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
@@ -12,7 +13,11 @@ const rsync        = require('gulp-rsync')
 
 function browsersync() {
 	browserSync.init({
-		server: { baseDir: 'app/' },
+		server: {
+			baseDir: 'app/',
+			middleware: ssi({ baseDir: 'app/', ext: '.html' })
+		},
+		tunnel: 'yousutename2', // Attempt to use the URL http://yousutename2.loca.lt
 		notify: false,
 		online: true
 	})
@@ -20,63 +25,63 @@ function browsersync() {
 
 function scripts() {
 	return src('app/js/app.js')
-	.pipe(webpack({
-		mode: 'production',
-		module: {
-			rules: [
-				{
-					test: /\.(js)$/,
-					exclude: /(node_modules)/,
-					loader: 'babel-loader',
-					query: {
-						presets: ['@babel/env']
+		.pipe(webpack({
+			mode: 'production',
+			module: {
+				rules: [
+					{
+						test: /\.(js)$/,
+						exclude: /(node_modules)/,
+						loader: 'babel-loader',
+						query: {
+							presets: ['@babel/env']
+						}
 					}
-				}
-			]
-		}
-	})).on('error', function handleError() {
-		this.emit('end')
-	})
-	.pipe(rename('app.min.js'))
-	.pipe(dest('app/js'))
-	.pipe(browserSync.stream())
+				]
+			}
+		})).on('error', function handleError() {
+			this.emit('end')
+		})
+		.pipe(rename('app.min.js'))
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
 }
 
 function styles() {
 	return src('app/sass/main.sass')
-	.pipe(sass({ outputStyle: 'compressed' }))
-	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-	.pipe(rename('app.min.css'))
-	.pipe(dest('app/css'))
-	.pipe(browserSync.stream())
+		.pipe(sass({ outputStyle: 'compressed' }))
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+		.pipe(rename('app.min.css'))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
 }
 
 function images() {
 	return src(['app/images/src/**/*'])
-	.pipe(newer('app/images/dist'))
-	.pipe(imagemin())
-	.pipe(dest('app/images/dist'))
-	.pipe(browserSync.stream())
+		.pipe(newer('app/images/dist'))
+		.pipe(imagemin())
+		.pipe(dest('app/images/dist'))
+		.pipe(browserSync.stream())
 }
 
 function deploy() {
 	return src('app/')
-	.pipe(rsync({
-		root: 'app/',
-		hostname: 'username@yousite.com',
-		destination: 'yousite/public_html/',
-		include: [/* '*.htaccess' */], // Included files to deploy,
-		exclude: [
-			'**/Thumbs.db',
-			'**/*.DS_Store',
-			'js/app.js',
-			'sass',
-		],
-		recursive: true,
-		archive: true,
-		silent: false,
-		compress: true
-	}))
+		.pipe(rsync({
+			root: 'app/',
+			hostname: 'username@yousite.com',
+			destination: 'yousite/public_html/',
+			include: [/* '*.htaccess' */], // Included files to deploy,
+			exclude: [
+				'**/Thumbs.db',
+				'**/*.DS_Store',
+				'js/app.js',
+				'sass',
+			],
+			recursive: true,
+			archive: true,
+			silent: false,
+			compress: true
+		}))
 }
 
 function startwatch() {
@@ -86,9 +91,9 @@ function startwatch() {
 	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
 }
 
-exports.scripts  = scripts
-exports.styles   = styles
-exports.images   = images
-exports.deploy   = deploy
-exports.assets   = series(styles, scripts, images)
-exports.default  = series(scripts, images, styles, parallel(browsersync, startwatch))
+exports.scripts = scripts
+exports.styles = styles
+exports.images = images
+exports.deploy = deploy
+exports.assets = series(styles, scripts, images)
+exports.default = series(scripts, images, styles, parallel(browsersync, startwatch))
