1f50fb6f60a25b4ec529de517aa3e4ecb22e2704
Алексей <agragregra@ya.ru>
2021-01-23 15:04:20 +0300

    Update

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 // import $ from 'jquery'; window.jQuery = $; window.$ = $ // import jQuery module (npm i jquery)
 
-// require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
+// require('~/app/libs/mmenu/dist/mmenu.js') // import vendor jQuery plugin example (not module)
 
 document.addEventListener('DOMContentLoaded', () => {
 
