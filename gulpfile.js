4ab3b146c7c761b8079a68f57a5c69c55b04ee21
Алексей <agragregra@ya.ru>
2021-04-13 16:53:52 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-let preprocessor = 'sass', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
+let preprocessor = 'styl', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
 		fileswatch   = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
 
 const { src, dest, parallel, series, watch } = require('gulp')
