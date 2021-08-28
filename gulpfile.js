ab1e25ab9379779c795cea99908ab34bbc6c699c
Алексей <agragregra@ya.ru>
2021-08-28 10:13:44 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,7 @@ function scripts() {
 function styles() {
 	return src([`app/styles/${preprocessor}/*.*`, `!app/styles/${preprocessor}/_*.*`])
 		.pipe(eval(`${preprocessor}glob`)())
-		.pipe(eval(preprocessor)())
+		.pipe(eval(preprocessor)({ 'include css': true }))
 		.pipe(postCss([
 			autoprefixer({ grid: 'autoplace' }),
 			cssnano({ preset: ['default', { discardComments: { removeAll: true } }] })
